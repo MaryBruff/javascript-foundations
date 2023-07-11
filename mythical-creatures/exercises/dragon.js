6b7e9b0c0b4c8b7f86def3902e789ae68aa11307
Mary Bruff <marybruff5@gmail.com>
2023-07-11 11:09:34 -0600

    unskip all tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,47 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+ dragon.timesEaten++
+ if (dragon.timesEaten >= 3) {
+  dragon.hungry = false
+ }
+ return dragon 
+}
+
+function findFireBreathers(dragon) {
+  var fireBreathers = []
+  for ( var i = 0; i < dragon.length; i++) {
+    if (dragon[i].temperment === 'aggressive'){
+      fireBreathers.push(dragon[i])
+    }
+  }
+  return fireBreathers
+}
+
+//conditional to check temperment
+//push that data into empty array called firebreathers 
+
+//.length 
+//for loop to find temperment 
+//push that into new array and return array
+// pass in dragon 
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
